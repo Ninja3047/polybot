2be65e90c366cd54be7c6f22a7c1b356cd924010
William Tan <williamjefftan@gmail.com>
2016-01-01 17:59:32 -0500

    use opensearch api for more robust wikipediaing

--- chat_modules/wiki.js
+++ chat_modules/wiki.js
@@ -5,14 +5,19 @@ client.chat.on("message", function(ev, msg) {
 	var match = msg.match(/\.wiki (.+)/);
 	if (match) {
 		client.startTyping(ev);
-		request("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exchars=420&explaintext&redirects=&titles=" + match[1], function(error, response, body) {
+		request("https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&redirects=resolve&format=json&search=" + match[1], function(error, response, body) {
 			var res = JSON.parse(body);
-			var link = "https://en.wikipedia.org/wiki/" + match[1];
-			var summary = res.query.pages[Object.keys(res.query.pages)[0]].extract;
+			var link = res[3][0];
+			var summary = res[2][0];
 			var linkbuilder = new client.Client.MessageBuilder();
-			var segments = linkbuilder.text(summary).text(" ").link("[full]", link).toSegments();
+			var segments;
+			if(summary.match(/.+may refer to:/))
+				segments = linkbuilder.link("[Ambiguous]", link).toSegments();
+			else segments = linkbuilder.text(summary).text(" ").link("[full]", link).toSegments();
+
 			if(summary) client.replySegments(ev, segments);
 			else client.replyMessage(ev, "no results found");
+
 			client.stopTyping(ev);
 		});
 	}
